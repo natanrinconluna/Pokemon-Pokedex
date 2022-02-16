6a2287c64de152cd832270bb2926ce032d3528d1
Natan Rincon Luna <rincon.luna.natan@gmail.com>
2022-02-16 11:31:29 -0800

    initial commit

---
+++ FrontJS.js
@@ -0,0 +1,9 @@
+import Pokedex from 'pokedex-promise-v2';
+const options = {
+  protocol: 'https',
+  hostName: 'localhost:443',
+  versionPath: '/api/v2/',
+  cacheLimit: 100 * 1000, // 100s
+  timeout: 5 * 1000 // 5s
+}
+const P = new Pokedex(options);
